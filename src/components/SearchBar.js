35cc7755c7486f9188ec621b12f3caae13ddde68
deiu25 <d3iu25@gmail.com>
2023-03-12 21:35:32 +0200

    comp and fetching data

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = ({ onSubmit }) => {
+  const handleClick = () => {
+    onSubmit("cars");
+  };
+
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default SearchBar;
